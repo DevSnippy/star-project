97f5ff9449ed74a769bb587484e309f2505d4352
DevSnippy <98705990+DevSnippy@users.noreply.github.com>
2022-03-12 20:39:52 +0200

    Add files via upload

--- untitled-star-project/src/components/NavBar.js
+++ untitled-star-project/src/components/NavBar.js
@@ -1,14 +1,15 @@
 import { FaGithub , FaUserAstronaut ,FaMeteor , FaLinkedin} from 'react-icons/fa';
 
+
 function NavBar () {
     return (
         <nav className=''>
  <div className="flex justify-around items-center text-5xl">
 
-<ul className='flex  hover:scale-125 hover:bg-white duration-300 rounded-full'><FaUserAstronaut/></ul>
-<ul className='flex  hover:scale-125 hover:bg-white duration-300 rounded-full'><a href='https://github.com/DevSnippy'><FaGithub/></a></ul>
-<ul className='flex  hover:scale-125 hover:bg-white duration-300 rounded-full'><FaMeteor/></ul>
-<ul className='flex  hover:scale-125 hover:bg-white duration-300 rounded-full'><FaLinkedin/></ul>
+<ul className='flex  hover:scale-125 hover:bg-white duration-500 rounded-full '><FaUserAstronaut/></ul>
+<ul className='flex  hover:scale-125 hover:bg-white duration-500 rounded-full'><a href='https://github.com/DevSnippy'><FaGithub/></a></ul>
+<ul className='flex  hover:scale-125 hover:bg-white duration-500 rounded-full'><FaMeteor/></ul>
+<ul className='flex  hover:scale-125 hover:bg-white duration-500 rounded-full'><FaLinkedin/></ul>
 </div>
 </nav>
     )
